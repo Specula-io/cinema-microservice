398d074ad114c2662b9884d0bb05c36f22285b9c
Luca Alexandru <luca.p.alexandru@gmail.com>
2021-04-13 17:16:50 +0300

    fix api definitions

--- booking-service/src/models/booking.model.js
+++ booking-service/src/models/booking.model.js
@@ -2,10 +2,7 @@ const bookingSchema = (joi) => ({
   city: joi.string(),
   cinema: joi.string(),
   schedule: joi.date().min('now'),
-  movie: joi.object().keys({
-    title: joi.string(),
-    format: joi.string()
-  }),
+  movie: joi.string(),
   cinemaRoom: joi.number(),
   seats: joi.array().items(joi.string()).single(),
   totalAmount: joi.number()
